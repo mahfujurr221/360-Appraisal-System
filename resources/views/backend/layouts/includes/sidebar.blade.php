a05b942421207e71cea7e8c9598bcba1531e9c4a
mahfujurr221 <mahfujurr221@gmail.com>
2023-10-12 01:01:37 +0600

    role permission for employee addes

--- resources/views/backend/layouts/includes/sidebar.blade.php
+++ resources/views/backend/layouts/includes/sidebar.blade.php
@@ -20,47 +20,49 @@
                 <div class="menu-title">Dashboard</div>
             </a>
         </li>
-        <li>
-            <a href="{{ route('admin.index') }}">
-                <div class="parent-icon">
-                    <i class='bx bx-user'></i>
-                </div>
-                <div class="menu-title">Admins</div>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('employee.index') }}">
-                <div class="parent-icon">
-                    <i class='bx bx-user'></i>
-                </div>
-                <div class="menu-title">Employees</div>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('role.index') }}">
-                <div class="parent-icon">
-                    <i class='bx bx-edit-alt'></i>
-                </div>
-                <div class="menu-title">Roles</div>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('department.index') }}">
-                <div class="parent-icon">
-                    <i class='bx bx-buildings'></i>
-                </div>
-                <div class="menu-title">Department</div>
-            </a>
-        </li>
+        @if (auth()->user()->role_id == 1)
+            <li>
+                <a href="{{ route('admin.index') }}">
+                    <div class="parent-icon">
+                        <i class='bx bx-user'></i>
+                    </div>
+                    <div class="menu-title">Admins</div>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('employee.index') }}">
+                    <div class="parent-icon">
+                        <i class='bx bx-user'></i>
+                    </div>
+                    <div class="menu-title">Employees</div>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('role.index') }}">
+                    <div class="parent-icon">
+                        <i class='bx bx-edit-alt'></i>
+                    </div>
+                    <div class="menu-title">Roles</div>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('department.index') }}">
+                    <div class="parent-icon">
+                        <i class='bx bx-buildings'></i>
+                    </div>
+                    <div class="menu-title">Department</div>
+                </a>
+            </li>
 
-        <li>
-            <a href="{{ route('designation.index') }}">
-                <div class="parent-icon">
-                    <i class='bx bx-arrow-to-right'></i>
-                </div>
-                <div class="menu-title">Designation</div>
-            </a>
-        </li>
+            <li>
+                <a href="{{ route('designation.index') }}">
+                    <div class="parent-icon">
+                        <i class='bx bx-arrow-to-right'></i>
+                    </div>
+                    <div class="menu-title">Designation</div>
+                </a>
+            </li>
+        @endif
 
     </ul>
     <!--end navigation-->
